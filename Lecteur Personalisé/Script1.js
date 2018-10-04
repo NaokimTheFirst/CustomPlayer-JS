7faa905b1ee346f5c8aab8ade4077418800e146a
Youmustkidding <32686212+Youmustkidding@users.noreply.github.com>
2018-10-04 21:57:19 +0200

    Barre de progression cliquable

--- Lecteur Personalisé/Script1.js
+++ Lecteur Personalisé/Script1.js
@@ -24,23 +24,64 @@ function controlVolume(value) {
 }
 
 function update(player) {
-    var duration = player.duration;    // Durée totale
-    var time = player.currentTime; // Temps écoulé
+    var time = player.currentTime;
+    var duration = player.duration;
     var fraction = time / duration;
     var percent = Math.ceil(fraction * 100);
 
     var progress = document.querySelector('#progressBar');
 
     progress.style.width = percent + '%';
-    var time = format(Math.floor(player.currentTime / 60)) + " : " + format(Math.floor(player.currentTime) % 60);
-    var duration = format(Math.floor(player.duration / 60)) + " : " + format(Math.floor(player.duration) % 60);
-    document.getElementById('progressTime').textContent = time + " / " + duration;
+    var stringTime = format(time);
+    var stringDuration = format(duration);
+    document.getElementById('progressTime').textContent = stringTime + " / " + stringDuration;
 }
 
 
-function format(num) {
-    if (num < 10) {
-        num = "0" + num;
+function format(time) {
+    var minutes = Math.floor(time / 60);
+    var seconds = Math.floor(time) % 60;
+    
+    if (minutes < 10) {
+        minutes = "0" + minutes;
     }
-    return num;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    result = minutes +" : "+seconds;
+    return result;
+}
+
+function getMousePosition(event) {
+    return {
+        x: event.pageX,
+        y: event.pageY
+    };
+}
+
+function getPosition(element){
+    var top = 0;
+    var left = 0;
+
+    do{
+        top += element.offsetTop;
+        left += element.offsetLeft;
+    }while (element = element.offsetParent);
+
+    return{x: left, y:top};
+}
+
+function clickProgress(control, event) {
+    var parent = getPosition(control);    // La position absolue de la progressBar
+    var target = getMousePosition(event); // L'endroit de la progressBar où on a cliqué
+    var player = document.querySelector("video");
+   console.log("called");
+    var x = target.x - parent.x; 
+    var wrapperWidth = document.querySelector('#progressBarControl').offsetWidth;
+    
+    var percent = Math.ceil((x / wrapperWidth) * 100);    
+    var duration = player.duration;
+    
+    player.currentTime = (duration * percent) / 100;
 }
