b0c567aa9b93e95bceff0304ee3cfd806ad37a9c
Youmustkidding <32686212+Youmustkidding@users.noreply.github.com>
2018-10-14 18:17:45 +0200

    Liste de lecture
    - Changement du readme : ajout des auteurs, description du sujet et cahier des charges.
    - Ajout d'une image dans les éléments de la liste de lecture
    - Meilleure gestion de la liste de lecture : la playlist du JS et la liste de div HTML correspondent bien l'une à lautre.
    - Gestion des situations de changemet de la tête de lecture en cours de lecture (suppression, changement d'ordre)

--- Lecteur Personalisé/Script1.js
+++ Lecteur Personalisé/Script1.js
@@ -1,27 +1,29 @@
 ﻿var playlist = [];
-/*
-window.onload = function(){
-    playlist = new Array();
-}*/
-function customPlay(boutonID) {
-    var player = document.querySelector("video");
 
-    if (player.paused) {
-        player.play();
-        boutonID.textContent = "||"; 
-    } else {
-        player.pause();
-        boutonID.textContent = "►";
+function customPlay() {
+    var player = document.querySelector("video");
+    button = document.getElementById('play');
+
+    if(player.getAttribute("src")!=""){
+        if (player.paused) {
+            player.play();
+            button.textContent = "||"; 
+        } else {
+            player.pause();
+            button.textContent = "►";
+        }
     }
-    
 }
 
 function customStop(){
     var player = document.querySelector("video");
-    player.pause();
-    player.currentTime = 0;
 
-    document.getElementById('play').textContent = "►";
+    if(player.getAttribute("src")!=""){
+        player.pause();
+        player.currentTime = 0;
+
+        document.getElementById('play').textContent = "►";
+    }
 }
 
 function controlVolume(value) {
@@ -79,84 +81,106 @@ function getPosition(element){
 }
 
 function clickProgress(control, event) {
-    var parent = getPosition(control);    // La position absolue de la progressBar
-    var target = getMousePosition(event); // L'endroit de la progressBar où on a cliqué
     var player = document.querySelector("video");
-    var x = target.x - parent.x; 
-    var wrapperWidth = document.querySelector('#progressBarControl').offsetWidth;
-    
-    var percent = Math.ceil((x / wrapperWidth) * 100);    
-    var duration = player.duration;
-    
-    player.currentTime = (duration * percent) / 100;
+    if(player.getAttribute("src")!=""){
+        //On enregistre l'état du lecteur pause ou play
+        var status = player.paused;
+        //Si le lecteur est sur play, on le stop
+        if(status === false){
+            player.pause();
+        }
+
+        var parent = getPosition(control);    // La position absolue de la progressBar
+        var target = getMousePosition(event); // L'endroit de la progressBar où on a cliqué
+        var x = target.x - parent.x; 
+        var wrapperWidth = document.querySelector('#progressBarControl').offsetWidth;
+        
+        var percent = Math.ceil((x / wrapperWidth) * 100);    
+        var duration = player.duration;
+        
+        player.currentTime = (duration * percent) / 100;
+        
+        //Si le lecteur était sur play, on le relance
+        if(status === false){
+            player.play();
+        }
+    }
 }
 function show(elem) {
     elem.hidden = false;
 }
 
 function getVideo(elem) {
+    //On cache le champ qui permet de rentrer un url
     elem.hidden = true;
-    var lien = document.getElementById('lienXML').value;
-    var lien = 'https://cors-anywhere.herokuapp.com/' + lien;
 
+    //On enregistre la taille de la liste avant modification
+    var previousLength = playlist.length;
+
+    //On récupère l'URL et on le modifie pour pouvoir y accéder
+    var lien = document.getElementById('lienXML').value;
+    lien = 'https://cors-anywhere.herokuapp.com/' + lien;
+
+    //On exécute notre requête pour récupérer tous les "item" du fichier XML
     var request = new XMLHttpRequest();
     request.open("GET", lien);
     request.addEventListener('readystatechange', function () {
         if (this.readyState === 4 && this.status === 200) {
             var xml = this.responseXML;
-            playlist = xml.getElementsByTagName("item");
-            updateVideoList();
+            listElem =xml.getElementsByTagName("item")
+            for (var i = 0; i < listElem.length; i++) {
+                playlist.push(listElem[i]);
+            }
+            updateVideoList(previousLength);
         }
     })
     request.send();
 }
 
+//Fonction qui ajoute des div à la page web
+function updateVideoList(previousLength) {
+    //Pour chaque nouvel élément de la liste on ajoute un div
+    for (var i = previousLength; i < playlist.length; i++) {
+        newDiv = createVideoElem();
+        newVideo(playlist[i], newDiv);
+    }
 
-function updateVideoList() {
-    for (var i = 0; i < playlist.length; i++) {
-        //console.log(playlist[i].getElementsByTagName("enclosure")[0].textContent);
-        //console.log(playlist[i].getElementsByTagName("itunes:image")[0].href);
-        newVideo(playlist[i]);
+    //Si la liste était vide auparavant on démarre la première vidéo
+    if(previousLength == 0){
+        StartFirstVid();
     }
 }
 
-function addVideo() {
-    var list = document.getElementById("listeVideo");
-    if(list.children.length === 0){createVideoElem(list);}
-    else{newVideo(list);}
-}
 
-function newVideo(list){
-    var clone = list.children[0].cloneNode(true);
-    list.appendChild(clone);
-}
-
-function newVideo(item) {
-    var list = document.getElementById("listeVideo");
-    var clone = list.children[0].cloneNode(true);
+function newVideo(item, div) {
     var titre = item.getElementsByTagName("title")[0].textContent;
     var img = item.getElementsByTagName("itunes:image")[0].getAttribute("href");
 
-    clone.querySelector("h2").innerHTML = titre;
-    clone.querySelector("image").setAttribute("href",img);
-    list.appendChild(clone);
+    div.querySelector("h3").innerHTML = titre;
+    div.querySelector("img").setAttribute("src",img);
 }
 
-//Création d'une div de vignette vidéo si aucune n'existe
-function createVideoElem(list){
-    list.innerHTML = "";
+//Création d'une div de vignette vidéo
+function createVideoElem(){
+    var list = document.getElementById("listeVideo");
+
+    //On enlève le message qui explique que la liste est vide
+    if(list.childElementCount == 0){
+        list.innerHTML = "";
+    }
+    
 
     var newDiv = document.createElement("div");
     list.appendChild(newDiv);
     newDiv.setAttribute("class","vignette");
 
-    var title = document.createElement("h2");
+    var title = document.createElement("h3");
     title.innerHTML ="default";
     newDiv.appendChild(title);
 
-    var image = document.createElement("image");
-    image.setAttribute("href", "null");
-    image.setAttribute("width","150px");
+    var image = document.createElement("img");
+    image.setAttribute("src", "null");
+    image.setAttribute("class","IMGvignette")
     newDiv.appendChild(image);
 
     var button1 = document.createElement("input");
@@ -174,8 +198,10 @@ function createVideoElem(list){
     var button3 = document.createElement("input");
     button3.setAttribute("type","button");
     button3.setAttribute("value","Delete");
-    button3.setAttribute("onclick", "deleteVid(this)");
+    button3.setAttribute("onclick", "RemoveVid(this)");
     newDiv.appendChild(button3);
+
+    return newDiv;
 }
 
 //Déplace l'élément vers le haut dans la liste
@@ -185,8 +211,19 @@ function moveUp(elem){
     //Si c'est le premier de la liste, quitte la fonction
     if(elem.previousSibling === null){return;}
 
+    //On récupère son index
+    var i = whichChild(elem);
+    //On l'échange de place dans la playlist
+    var temp  = playlist[i];
+    playlist[i] = playlist[i-1];
+    playlist[i-1]= temp;
     //On insère l'élément avant celui qui le précède
     elem.parentNode.insertBefore(elem,elem.previousSibling);
+
+    //Si on change la vidéo courrante d'une place, on démarre la nouvelle vidéo 0
+    if(i-1===0){
+        StartFirstVid();
+    }
 }
 
 
@@ -197,19 +234,67 @@ function moveDown(elem){
     //Si c'est le dernier de la liste, quitte la fonction
     if(elem.nextSibling === null){return;}
 
+    //On récupère son index
+    var i = whichChild(elem);
+    //On l'échange de place dans la playlist
+    var temp  = playlist[i];
+    playlist[i] = playlist[i+1];
+    playlist[i+1]= temp;
     //On insère l'élément après celui qui le suit
     elem.parentNode.insertBefore(elem,elem.nextSibling.nextSibling);
+    //Si on change la vidéo courrante d'une place, on démarre la nouvelle vidéo 0
+    if(i===0){
+        StartFirstVid();
+    }
+}
+
+//Supprime la vidéo précédente et démarre la suivante
+function StartNextVid(){
+    DeleteFirstVid();
+    if(playlist.length != 0){
+        StartFirstVid();
+    }
+}
+
+//Démarre la vidéo à l'index 0
+function StartFirstVid(){
+    var player = document.querySelector("video");
+    var lien = playlist[0].getElementsByTagName("enclosure")[0].getAttribute("url");
+    //Change le lien de la vidéo
+    player.setAttribute("src",lien);
+    //Démarre la video
+    customPlay();
 }
 
 //Supprime une vidéo dans la liste d'attente
-function deleteVid(elem){
+function RemoveVid(elem){
     var elem = elem.parentNode;
     var list = document.getElementById("listeVideo");
-    list.removeChild(elem);
+
+    //On compte pour savoir quelle est la position de la vidéo dans la liste de div
+    var  i= whichChild(elem);
+
+    //Si on supprime la vidéo courrante
+    if(i===0){
+        StartNextVid();
+    } else {
+    //Sinon on supprime la vidéo de la liste de Div et de la playlist
+        list.removeChild(elem);
+        playlist.splice(i,1);   //Param1 : Position, param2 : nb d'elem a supprimer
+    }   
 }
 
-//Supprime une vidéo dans la liste d'attente
-function deleteVid(){
+//Fonction qui retourne l'index de l'élement dans la liste de son parent
+function whichChild(elem){
+    var  i= 0;
+    while((elem=elem.previousSibling)!=null) ++i;
+    return i;
+}
+
+//Supprime le premier élément de la liste
+function DeleteFirstVid(){
     var list = document.getElementById("listeVideo");
+    //Supprime le premier élément de la liste et décale tous les autres élément à l'index inférieur
+    playlist.shift();
     list.removeChild(list.children[0]);
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # CustomPlayer-JS
+
+Ce projet consiste à créer en HTML+JS+CSS un lecteur multimédia (audio/vidéo), permettant la gestion d’une liste de lecture.
+
+Cahier des charges : 
+- Votre application utilisera une unique balise <video>, dont vous n’utilisez pas l’attribut controls. Au lieu de cela, vous créerez votre propre interface de contrôle.
+- Votre application comprendra également une liste de lecture, dans laquelle il doit être possible d’ajouter, supprimer ou déplacer des éléments.
+- Lorsque la lecture d’un élément se termine, celui-ci doit disparaître de la liste de lecture, et la lecture de l’élément suivant doit automatiquement commencer.
+- Votre application doit également proposer de remplir la liste de lecture à partir de l’URL d’un Podcast. Dans ce cas, les informations du podcast (par exemple: titre des éléments, image de couverture pour les podcasts audio) seront utilisés dans l’interface.
+- Vous ne devez pas utiliser de bibliothèque ou de framework:en:</i>.
+- Votre code doit passer sans erreur ni avertissement le valideur du W3C pour HTML et CSS, et JSHint pour le code Javascript.
+- Le soin que vous apporterez à l’ergonomie et à la présentation de votre application rentrera également en ligne de compte dans la notation.
+
+Développé par : Prigent Benoit et Peyrot Romain
+
+Flux RSS fonctionnel : http://feeds.radiokawa.com/podcast_faster-than-light.xml
